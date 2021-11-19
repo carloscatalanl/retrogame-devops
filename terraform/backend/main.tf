bcac6b90d9ac67f342b2f3e384226dd7b4701daa
CarlosCatalan <carloscatalan@CarlosCatalan-MacBook-Pro.local>
2021-11-18 22:48:31 -0700

    terraform path

--- terraform/backend/main.tf
+++ terraform/backend/main.tf
@@ -16,20 +16,20 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-// resource "azurerm_virtual_network" "virtualnet" {
-//   name                = "tfBeVN"
-//   address_space       = ["10.0.0.0/16"]
-//   location            = azurerm_resource_group.rg.location
-//   resource_group_name = azurerm_resource_group.rg.name
-// }
+resource "azurerm_virtual_network" "virtualnet" {
+  name                = "tfBeVN"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
-// resource "azurerm_subnet" "subnet" {
-//   name                 = "tfBeSN"
-//   resource_group_name  = azurerm_resource_group.rg.name
-//   virtual_network_name = azurerm_virtual_network.virtualnet.name
-//   address_prefixes     = ["10.0.2.0/24"]
-//   service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
-// }
+resource "azurerm_subnet" "subnet" {
+  name                 = "tfBeSN"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.virtualnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
+}
 
 resource "azurerm_storage_account" "asa" {
   name                     = var.asa_name
@@ -39,11 +39,11 @@ resource "azurerm_storage_account" "asa" {
   account_replication_type = "GRS"
   allow_blob_public_access = true
 
-  //  network_rules {
-  //   default_action             = "Deny"
-  //   ip_rules                   = ["100.0.0.1"]
-  //   virtual_network_subnet_ids = [azurerm_subnet.subnet.id]
-  // }
+   network_rules {
+    default_action             = "Deny"
+    ip_rules                   = ["100.0.0.1"]
+    virtual_network_subnet_ids = [azurerm_subnet.subnet.id]
+  }
 }
 
 resource "azurerm_storage_container" "asc" {
